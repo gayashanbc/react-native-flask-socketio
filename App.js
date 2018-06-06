b2b92d6d1323cd3f76026616946c5c6e07f311b3
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-06 22:01:19 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import io from 'socket.io-client';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.socket = io(`http://10.0.3.2:5000/chat`);
+
+    this.socket.on('connect', () => {
+      console.log('connected')
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
